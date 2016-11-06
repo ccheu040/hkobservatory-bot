5908e8d913c681c8ad529b1c54be525747d4d635
ccheu040 <ccheu040@uottawa.ca>
2016-11-06 16:32:40 -0500

    Added "/tellme warning" command

--- hkobservatory_bot.py
+++ hkobservatory_bot.py
@@ -6,23 +6,35 @@ bot_token = "228404369:AAF_vs8a2cDGiaAVrrLy8eDRzcAWobkX8qg"
 updater = telegram.ext.Updater(token=bot_token)
 dispatcher = updater.dispatcher
 
+
+# Sends information from HK Observatory about specified topic
 def tellme(bot, update, args):
     topic = " ".join(args)
 
     if not args:
-        bot.sendMessage(chat_id=update.message.chat_id, text="I need a topic to tell you about! See /topic for a list of available topics.")
+        message = "I need a topic to tell you about! See /topic for a list of available topics."
+
+    # Sends information about the current weather from RSS feed
     elif topic == "current":
         rss = feedparser.parse("http://rss.weather.gov.hk/rss/CurrentWeather.xml")
-        soup = bs4.BeautifulSoup(rss.entries[0].summary,"html.parser")
+        current = bs4.BeautifulSoup(rss.entries[0].summary, "html.parser")
 
-        soup.table.decompose()
-        soup.p.p.next_sibling.replace_with("")
+        current.table.decompose()
+        current.p.p.next_sibling.replace_with("")
         # soup.find("font", size="-1").previous_sibling.previous_sibling.previous_sibling.replace_with("")
-        current = []
-        for string in soup.stripped_strings:
-            current.append(" ".join(string.split()))
-        current = "\n".join(current)
-        bot.sendMessage(chat_id=update.message.chat_id, text=current)
+        message = []
+        for string in current.stripped_strings:
+            message.append(" ".join(string.split()))
+        message = "\n".join(message)
+
+    # Sends information about current warnings from RSS feed
+    elif topic == "warning":
+        rss = feedparser.parse("http://rss.weather.gov.hk/rss/WeatherWarningBulletin.xml")
+        warning = bs4.BeautifulSoup(rss.entries[0].summary, "html.parser")
+        message = warning.get_text()
+
+    bot.sendMessage(chat_id=update.message.chat_id, text=message)
+
 
 tellme_handler = telegram.ext.CommandHandler("tellme", tellme, pass_args=True)
 dispatcher.add_handler(tellme_handler)
